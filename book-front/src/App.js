1e223fa4dba48c393c69e23101ef1e26bc62d024
CharbelAllawi <charbelallawiwork@gmail.com>
2023-08-23 18:13:42 +0300

    Front <> Default Clean

---
+++ book-front/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Login_Signup from './components/Login_Signup';
+
+function App() {
+  return (
+    <div className="App">
+      <Login_Signup />
+    </div>
+  );
+}
+
+export default App;
